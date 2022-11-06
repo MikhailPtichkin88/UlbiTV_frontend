e510d83d15ba3d00ef164dd4fb83e7bcb961e83d
MikhailPtichkin88 <mikhailptichkin1988@gmail.com>
2022-11-06 17:36:25 +0300

    add classNames function helper

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import { Link, Route, Routes } from "react-router-dom";
 import { MainPageAsync } from "./pages/MainPage/MainPage.async";
 import { AboutPageAsync } from "./pages/AboutPage/AboutPage.async";
 import { useTheme } from "./theme/useTheme";
+import { classNames } from "./helpers/classNames/classNames";
 
 export enum Theme {
   LIGHT = "light",
@@ -14,7 +15,7 @@ export enum Theme {
 export const App = () => {
   const { theme, toggleTheme } = useTheme();
   return (
-    <div className={`app ${theme}`}>
+    <div className={classNames('app', {}, [theme])}>
       <button onClick={toggleTheme}>Toggle theme</button>
       <Link to="/">Главная</Link>
       <Link to="/about">О сайте</Link>
---
+++ src/helpers/classNames/classNames.ts
@@ -0,0 +1,10 @@
+type Mods = Record < string, boolean | string >
+  export function classNames(cls: string, mods: Mods, additional: string[]): string {
+    return [
+      cls,
+      ...additional,
+      Object.entries(mods)
+      .filter(([className, value]) => Boolean(value))
+      .map(([className, value]) => className)
+    ].join(' ')
+  }
