1402dd32aa92a5eaafb6407bbfc2e25240c34f69
MikhailPtichkin88 <mikhailptichkin1988@gmail.com>
2022-11-28 22:39:36 +0300

    add webpack-bundle-analizer

--- config/build/buildPlugins.ts
+++ config/build/buildPlugins.ts
@@ -3,11 +3,12 @@ import HtmlWebpackPlugin from "html-webpack-plugin";
 import MiniCssExtractPlugin from "mini-css-extract-plugin";
 import webpack from "webpack";
 import { BuildOptions } from "./types/config";
+import { BundleAnalyzerPlugin } from "webpack-bundle-analyzer";
+
 export function buildPlugins({
   isDev,
   paths,
 }: BuildOptions): webpack.WebpackPluginInstance[] {
-
   const plugins = [
     new HtmlWebpackPlugin({
       template: paths.html,
@@ -18,12 +19,15 @@ export function buildPlugins({
       chunkFilename: "css/[name].[contenthash:8].css",
     }),
     new webpack.DefinePlugin({
-      __IS_DEV__: JSON.stringify(isDev)
+      __IS_DEV__: JSON.stringify(isDev),
     }),
-  ]
-  if(isDev){
-    plugins.push( new webpack.HotModuleReplacementPlugin())
-    plugins.push( new ReactRefreshWebpackPlugin())
+    new BundleAnalyzerPlugin({
+      openAnalyzer: false,
+    }),
+  ];
+  if (isDev) {
+    plugins.push(new webpack.HotModuleReplacementPlugin());
+    plugins.push(new ReactRefreshWebpackPlugin());
   }
-  return plugins
+  return plugins;
 }
