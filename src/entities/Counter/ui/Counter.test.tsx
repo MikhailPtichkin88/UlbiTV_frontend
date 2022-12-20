85f46f714822e27fa5c06b693bce6e1fb43abaa9
MikhailPtichkin88 <mikhailptichkin1988@gmail.com>
2022-12-20 22:28:47 +0300

    fix bugs

--- src/entities/Counter/ui/Counter.test.tsx
+++ src/entities/Counter/ui/Counter.test.tsx
@@ -1,6 +1,6 @@
 import {  fireEvent, screen } from "@testing-library/react";
 import { componentRender } from "shared/lib/tests/componentRender/componentRender";
-import { Counter } from "./Counter";
+import { Counter } from "entities/Counter/ui/Counter";
 
 describe("Counter", () => {
     test("test", () => {
--- src/entities/Counter/ui/Counter.tsx
+++ src/entities/Counter/ui/Counter.tsx
@@ -1,9 +1,7 @@
-import { StateSchema } from 'app/providers/StoreProvider/config/StateSchema'
 import { useDispatch, useSelector } from 'react-redux'
 import { Button } from 'shared/ui/Button/Button'
 import { getCounterValue } from '../model/selectors/getCounterValue/getCounterValue'
 import { counterActions } from '../model/slice/CounterSlice'
-
 
 export const Counter = () => {
     const dispatch = useDispatch()
