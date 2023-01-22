8baddec0d106378fda429549f3b1758f60174c5a
MikhailPtichkin88 <mikhailptichkin1988@gmail.com>
2023-01-22 23:38:01 +0300

    add asyncThunk tests

--- src/features/AuthByUserName/model/services/loginByUserName/loginByUserName.test.ts
+++ src/features/AuthByUserName/model/services/loginByUserName/loginByUserName.test.ts
@@ -3,27 +3,61 @@ import { Dispatch } from "@reduxjs/toolkit";
 import { StateSchema } from "app/providers/StoreProvider";
 import axios from "axios";
 import { userActions } from "entities/User";
+import { TestAsyncThunk } from "shared/lib/tests/TestAsyncThunk/TestAsyncThunk";
 import { loginByUserName } from "./loginByUserName";
 
 jest.mock('axios')
 const mockedAxios = jest.mocked(axios, true)
 
 describe('loginByUserName.test', () => { 
-  let dispatch: Dispatch
-  let getState: ()=> StateSchema
+  // let dispatch: Dispatch
+  // let getState: ()=> StateSchema
 
-  beforeEach(()=>{
-    dispatch = jest.fn()
-    getState = jest.fn()
-  })
-  test("loginByUsername.test", async()=>{
+  // // beforeEach(()=>{
+  //   dispatch = jest.fn()
+  //   getState = jest.fn()
+  // })
+  // test("success login", async()=>{
+  //   const userValue = {username:"123", id:"1"}
+  //   mockedAxios.post.mockReturnValue(Promise.resolve({data:userValue}))
+  //   const action = loginByUserName({username:"123", password:"123"})
+  //   const result = await action(dispatch, getState, undefined)
+
+  //   expect(dispatch).toHaveBeenCalledWith(userActions.setAuthData(userValue))
+  //   expect(mockedAxios.post).toHaveBeenCalled()
+  //   expect(result.meta.requestStatus).toBe("fulfilled")
+  // })
+  // test('error login', async () => {
+  //   mockedAxios.post.mockReturnValue(Promise.resolve({ status: 403 }));
+  //   const action = loginByUserName({ username: '123', password: '123' });
+  //   const result = await action(dispatch, getState, undefined);
+    
+  //   expect(dispatch).toHaveBeenCalledTimes(2);
+  //   expect(mockedAxios.post).toHaveBeenCalled();
+  //   expect(result.meta.requestStatus).toBe('rejected');
+  //   expect(result.payload).toBe('error');
+  // });
+  test("success login", async()=>{
     const userValue = {username:"123", id:"1"}
     mockedAxios.post.mockReturnValue(Promise.resolve({data:userValue}))
-    const action = loginByUserName({username:"123", password:"123"})
-    const result = await action(dispatch, getState, undefined)
 
-    expect(dispatch).toHaveBeenCalledWith(userActions.setAuthData(userValue))
+    const thunk = new TestAsyncThunk(loginByUserName)
+    const result = await thunk.callThunk({username: "123", password:"123"})
+   
+    expect(thunk.dispatch).toHaveBeenCalledWith(userActions.setAuthData(userValue))
     expect(mockedAxios.post).toHaveBeenCalled()
     expect(result.meta.requestStatus).toBe("fulfilled")
   })
+
+  test('error login', async () => {
+    mockedAxios.post.mockReturnValue(Promise.resolve({ status: 403 }));
+
+    const thunk = new TestAsyncThunk(loginByUserName)
+    const result = await thunk.callThunk({username: "123", password:"123"})
+    
+    expect(thunk.dispatch).toHaveBeenCalledTimes(2);
+    expect(mockedAxios.post).toHaveBeenCalled();
+    expect(result.meta.requestStatus).toBe('rejected');
+    expect(result.payload).toBe('error');
+  });
 })
---
+++ src/features/AuthByUserName/model/slice/loginSlice.test.ts
@@ -0,0 +1,19 @@
+import { DeepPartial } from "@reduxjs/toolkit"
+import { StateSchema } from "app/providers/StoreProvider"
+import { LoginSchema } from "../types/loginSchema"
+import { loginActions, loginReducer } from "./loginSlice"
+
+describe("loginSlice.test", ()=> {
+  test("test set username", ()=>{
+        
+    const state: DeepPartial<LoginSchema> = {username:"123"}
+    expect(loginReducer(state as LoginSchema, loginActions.setUserName("123123"),
+    )).toMatchObject({ username: "123123" })
+  })
+  test("test set password", ()=>{
+        
+    const state: DeepPartial<LoginSchema> = {password:"123"}
+    expect(loginReducer(state as LoginSchema, loginActions.setPassword("123123"),
+    )).toMatchObject({ password: "123123" })
+  })
+})
