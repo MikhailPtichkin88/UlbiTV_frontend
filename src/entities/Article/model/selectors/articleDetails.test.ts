92a30ccff5409ded3151430f6868ca704f251e0c
MikhailPtichkin88 <mikhailptichkin1988@gmail.com>
2023-05-02 12:13:15 +0300

    add tests to Article entity

---
+++ src/entities/Article/model/selectors/articleDetails.test.ts
@@ -0,0 +1,50 @@
+import { StateSchema } from "app/providers/StoreProvider"
+import { getArticleDetailsData } from "./articleDetails"
+import { getArticleDetailsIsLoading } from "./articleDetails"
+import { getArticleDetailsError } from "./articleDetails"
+
+describe('getArticleDetailsData', ()=> {
+  test('should return data value', () => {
+    const data = {
+      id: "1",
+      title: "subtitle"
+    }
+    const state: DeepPartial<StateSchema> = {
+      articleDetails:{
+        data
+      }
+    }
+    expect(getArticleDetailsData(state as StateSchema)).toEqual(data)
+  })
+  test('should return empty state', () => {
+
+    const state: DeepPartial<StateSchema> = {}
+    expect(getArticleDetailsData(state as StateSchema)).toEqual(undefined)
+  })
+
+  test('should return isLoading', () => {
+  
+    const state: DeepPartial<StateSchema> = {
+      articleDetails:{
+        isLoading:true
+      }
+    }
+    expect(getArticleDetailsIsLoading(state as StateSchema)).toEqual(true)
+  })
+
+  test('should return empty state isLoading', () => {
+
+    const state: DeepPartial<StateSchema> = {}
+    expect(getArticleDetailsIsLoading(state as StateSchema)).toEqual(false)
+  })
+
+  test('should return error', () => {
+  
+    const state: DeepPartial<StateSchema> = {
+      articleDetails:{
+        error:'error'
+      }
+    }
+    expect(getArticleDetailsError(state as StateSchema)).toEqual('error')
+  })
+})
--- src/entities/Article/model/selectors/articleDetails.ts
+++ src/entities/Article/model/selectors/articleDetails.ts
@@ -1,6 +1,6 @@
 import { StateSchema } from "app/providers/StoreProvider";
 
 export const getArticleDetailsData = (state:StateSchema) => state.articleDetails?.data
-export const getArticleDetailsIsLoading = (state:StateSchema) => state.articleDetails?.isLoading
+export const getArticleDetailsIsLoading = (state:StateSchema) => state.articleDetails?.isLoading || false
 export const getArticleDetailsError = (state:StateSchema) => state.articleDetails?.error
 
