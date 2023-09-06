d78677ea605bcea9240bec9a128168f1673f8a2d
MikhailPtichkin88 <mikhailptichkin1988@gmail.com>
2023-09-06 22:56:35 +0300

    refactor

--- src/entities/Profile/model/types/profile.ts
+++ src/entities/Profile/model/types/profile.ts
@@ -1,14 +1,14 @@
-import { Currency } from 'entities/Currency/model/types/currency';
-import { Country } from 'entities/Country/model/types/country';
+import { Currency } from 'entities/Currency/model/types/currency'
+import { Country } from 'entities/Country/model/types/country'
 
 export interface Profile {
-    id?: string;
-    first?: string;
-    lastname?: string;
-    age?: number,
-    currency?: Currency,
-    country?: Country;
-    city?: string,
-    username?: string;
-    avatar?: string;
+  id?: string
+  first?: string
+  lastname?: string
+  age?: number
+  currency?: Currency
+  country?: Country
+  city?: string
+  username?: string
+  avatar?: string
 }
--- src/features/editableProfileCard/model/selectors/getProfileValidateErrors/getProfileValidateErrors.ts
+++ src/features/editableProfileCard/model/selectors/getProfileValidateErrors/getProfileValidateErrors.ts
@@ -1,3 +1,4 @@
-import { StateSchema } from 'app/providers/StoreProvider';
+import { StateSchema } from 'app/providers/StoreProvider'
 
-export const getProfileValidateErrors = (state: StateSchema) => state.profile?.validateErrors;
+export const getProfileValidateErrors = (state: StateSchema) =>
+  state.profile?.validateErrors
