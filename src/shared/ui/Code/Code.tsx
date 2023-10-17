6996dc131a38c34b50ffb73330dfeb59078ea4e8
mptichkin <apple@mptichkin.local>
2023-10-17 17:13:41 +0300

    fix errors

--- src/shared/ui/Code/Code.tsx
+++ src/shared/ui/Code/Code.tsx
@@ -3,7 +3,7 @@ import cls from './Code.module.scss'
 import { ReactNode, useCallback } from 'react'
 import { Button, ButtonTheme } from '../Button/Button'
 import CopyIcon from '@/shared/assets/icons/copy-20-20.svg'
-import { Icon } from '../Icon/ui/Icon'
+
 interface CodeProps {
   className?: string
   text: string
--- src/shared/ui/ThemeSwitcher/index.tsx
+++
@@ -1,2 +0,0 @@
-import { ThemeSwitcher } from './ui/ThemeSwitcher'
-export { ThemeSwitcher }
