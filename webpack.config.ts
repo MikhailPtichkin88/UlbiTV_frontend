4e1bdbd0b9cfd208aa9afdb588d7d12c3841ce7a
MikhailPtichkin <mikhailptichkin1988@gmail.com>
2023-11-08 22:16:56 +0300

    apiUrl deploy fix

--- webpack.config.ts
+++ webpack.config.ts
@@ -1,7 +1,17 @@
 import webpack from 'webpack'
 import path from 'path'
 import { buildWebpackConfig } from './config/build/buildWebpackConfig'
-import { BuildEnv, BuildPaths } from './config/build/types/config'
+import { BuildEnv, BuildMode, BuildPaths } from './config/build/types/config'
+
+function getApiUrl(mode: BuildMode, apiUrl?: string) {
+  if (apiUrl) {
+    return apiUrl
+  }
+  if (mode === 'production') {
+    return '/api'
+  }
+  return 'http://localhost:8000'
+}
 
 export default (env: BuildEnv) => {
   const paths: BuildPaths = {
@@ -15,8 +25,7 @@ export default (env: BuildEnv) => {
 
   const mode = env?.mode || 'development'
   const PORT = env?.port || 3000
-  const apiUrl = env?.apiUrl || 'http://localhost:8000'
-
+  const apiUrl = getApiUrl(mode, env?.apiUrl)
   const isDev = mode === 'development'
 
   const config: webpack.Configuration = buildWebpackConfig({
